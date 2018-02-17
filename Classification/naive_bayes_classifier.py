3295aadb897a5d70a6a724da478d3097ea446223
shubh2565 <shubh2565@users.noreply.github.com>
2018-02-17 20:49:12 +0530

    Add files via upload

---
+++ Classification/naive_bayes_classifier.py
@@ -0,0 +1,80 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
+
+#loading of dataset
+data = pd.read_csv('Social_Network_Ads.csv')
+print(data)
+X = data.iloc[ : , 2:4].values
+y = data.iloc[ : , 4].values
+print('\n{}'.format(X))
+print('\n{}'.format(y))
+
+
+#splitting the dataset into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print('\nX-train:\n{}'.format(X_train))
+print('\nX-test:\n{}'.format(X_test))
+print('\ny-train:\n{}'.format(y_train))
+print('\ny-test:\n{}'.format(y_test))
+
+
+#feature scaling so that no variable dominates over the other(s)
+feature = StandardScaler()
+X_train = feature.fit_transform(X_train)
+X_test = feature.transform(X_test)
+print('\nX-train after feature scaling:\n{}'.format(X_train))
+print('\nX-test after feature scaling:\n{}'.format(X_test))
+
+
+#fitting Naive Bayes model and predicting the test set results
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+y_pred = classifier.predict(X_test)
+
+
+#making confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+
+# Visualising the Training set results
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Naive Bayes (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Naive Bayes (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
